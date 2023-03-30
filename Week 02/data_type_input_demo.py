24c97d7b27b4a65271c76702374e2dd3a0511c69
nbedrossian <nishantafe@gmail.com>
2023-03-30 13:36:20 +1100

    Initial Commit

---
+++ Week 02/data_type_input_demo.py
@@ -0,0 +1,102 @@
+# Data Types
+
+# String
+# print("hello")
+# print(type("hello"))
+
+# Integer
+# num = 10
+# print(num)
+# print(type(num))
+
+#Float
+# flt = 10.57
+# print(type(flt))
+
+# year = "1976"
+# print(type(year))
+
+#Bool
+# print(type(True))
+# print(type(False))
+
+#Operators and Input
+
+# Add +
+# Subtract -
+
+# Multiply *
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = a * b
+#
+# print(c)
+
+# Divide /
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = a / b
+#
+# print(c)
+
+#Mod % Remainder
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = a % b
+#
+# print(c)
+
+#Floor Division //
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = a // b
+#
+# print(c)
+
+#Exponent **
+
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+# c = a ** b
+#
+# print(c)
+
+#String Cocantenation
+#Make variable for last name
+#Make variable for full name that adds first + last name
+#Make a variable for age
+#Print first name, last name and age
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+# age = input("Enter your age: ")
+
+# full_details = first_name + " " + last_name + " " + age
+# full_details = (first_name + " " + last_name + " " + "is" + " " + age + " " + "years old.")
+
+# full_details = (f"{first_name} {last_name} is {age} years old.")
+#
+# print(full_details)
+
+#Joe cool is 20 years old
+
+# name = input('Enter a name: ')
+# num = 10
+#
+# multiply_name = name * num
+# print(multiply_name)
+
+#
+# name = input('Enter a name: ')
+# age = int(input('Enter an age: '))
+#
+# print(f"Hi there {name}. You are {age}.")
+
+import math
+
+num_pi = math.pi
+
+print(num_pi)
+
